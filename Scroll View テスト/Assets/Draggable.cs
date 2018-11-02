1d4cd2370fe2c67f08cf79a183bd2e010c4ea41d
Unknown <b1802104@planet.kanazawa-it.ac.jp>
2018-11-02 19:34:14 +0900

    Scorll View テスト

---
+++ Scroll View テスト/Assets/Draggable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+
+    public Sprite spriteMae;
+    public Sprite spriteAto;
+
+    public GameObject DZ;
+
+    public void OnBeginDrag(PointerEventData eventDate)
+    {
+
+        Debug.Log("OnBeginDrag");
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        DropZone dz = DZ.GetComponent<DropZone>();
+        string location = dz.Decision();
+        Debug.Log(location);
+
+        if (location == "Hand")
+        {
+            this.gameObject.GetComponent<Image>().sprite = spriteMae;
+
+        }
+        else
+        {
+            this.gameObject.GetComponent<Image>().sprite = spriteAto;
+        }
+    }
+
+}
+
---
+++ Scroll View テスト/Assets/DropZone.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler
+{
+    public static string location;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + "was droped on " + gameObject.name);
+        location = gameObject.name;
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+
+    public string Decision()
+    {
+        Debug.Log("あげたいのは" + location);
+        return location;
+    }
+
+}
+
